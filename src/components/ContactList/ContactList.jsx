63c0c02e74a12d1c9a5e338245508484962106e4
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-06-15 15:23:43 +0100

    hw7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,35 +1,19 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Contact from '../Contact/Contact';
-import { selectFilteredContacts } from '../../redux/contactsSlice.js';
-import { fetchContacts, deleteContact } from '../../redux/contactsOps';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import css from './ContactList.module.css';
 
-const ContactList = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(selectFilteredContacts);
-  const loading = useSelector((state) => state.contacts.loading);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  const handleDelete = (id) => {
-    dispatch(deleteContact(id));
-  };
-
-  if (loading) return <p>Loading...</p>;
+export default function ContactList () {
+ const contacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.listStyle}>
       {contacts.map(contact => (
         <li className={css.itemStyle} key={contact.id}>
           <Contact contact={contact} />
-          {/* <button onClick={() => handleDelete(contact.id)}>Delete</button> */}
         </li>
       ))}
     </ul>
   );
-};
-
-export default ContactList;
+}
